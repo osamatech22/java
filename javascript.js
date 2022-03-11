1c89ff8a47ef6475466dd1676139a991aa7b9b85
osamatech22 <83569720+osamatech22@users.noreply.github.com>
2022-03-11 13:26:38 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 var d='<data:blog.url/>';
 d=d.replace(/.\/\/[^\/]/, '');
-location.href = 'https://net3arabi.com' + window.location.pathname;
+location.href = 'https://net3arabi.com/' + window.location.pathname;
