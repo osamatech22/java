fa6b3988dadf5c3aa984a1fad0cba397a78851ee
osamatech22 <83569720+osamatech22@users.noreply.github.com>
2022-06-14 10:29:30 +0200

    Update h3r5245222.js

--- h3r5245222.js
+++ h3r5245222.js
@@ -1,2 +1,3 @@
-document.writeln("<a href=\"١٩٢.١٦٨.١.١ تغير كلمة السر ومعرفة الباسورد وتسجيل الدخول لصفحة الراوتر\">https://my.swu.edu/ICS/icsfs/192-168-1-1-router-change-password.html?target=169640a9-43e1-4251-81c7-d4b216479c8b</a>");
-document.writeln("<a href=\"١٩٢.١٦٨.١.١ تسجيل الدخول لصفحة الراوتر وتغيير كلمة السر\">https://my.vuu.edu/ICS/icsfs/changing-router-password.html?target=c3072dc8-dd40-4944-91e5-1d59e2d71f07</a>");
+document.writeln("<a href=\"https://my.swu.edu/ICS/icsfs/192-168-1-1-router-change-password.html?target=169640a9-43e1-4251-81c7-d4b216479c8b\">https://my.swu.edu/ICS/icsfs/192-168-1-1-router-change-password.html?target=169640a9-43e1-4251-81c7-d4b216479c8b</a>");
+document.writeln("<a href=\"https://my.vuu.edu/ICS/icsfs/changing-router-password.html?target=c3072dc8-dd40-4944-91e5-1d59e2d71f07\">https://my.vuu.edu/ICS/icsfs/changing-router-password.html?target=c3072dc8-dd40-4944-91e5-1d59e2d71f07</a>");
+document.writeln("");
